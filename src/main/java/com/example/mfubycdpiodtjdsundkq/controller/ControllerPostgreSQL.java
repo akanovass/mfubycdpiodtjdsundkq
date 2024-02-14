b38e5e888f19d3e821fd7bad3f076cf093bf0a99
sh.akanova <akanovashyryn0@gmail.com>
2024-02-14 07:09:18 +0600

    created request get all and get one element

--- src/main/java/com/example/mfubycdpiodtjdsundkq/controller/ControllerPostgreSQL.java
+++ src/main/java/com/example/mfubycdpiodtjdsundkq/controller/ControllerPostgreSQL.java
@@ -4,6 +4,7 @@ import com.example.mfubycdpiodtjdsundkq.DTO.RegisterDTO;
 import com.example.mfubycdpiodtjdsundkq.services.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,14 @@ public class ControllerPostgreSQL {
     private RegisterService registerService;
 
     @GetMapping
-    public List<RegisterDTO> allRegister(){
+    public List<RegisterDTO> getAllRegister(){
         return registerService.getRegisters();
+
+    }
+
+    @GetMapping("{id}")
+    public RegisterDTO getRegister( @PathVariable(name = "id") Long id){
+        return registerService.getRegister(id);
 
     }
 }
